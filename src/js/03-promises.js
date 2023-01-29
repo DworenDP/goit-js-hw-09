37dc6567cea48a7f863478a74be6bf9c7131857c
DworenDP <cuda.jaro@gmail.com>
2023-01-29 17:56:58 +0200

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from "notiflix";
+
+const formEl = document.querySelector(".form");
+
+formEl.addEventListener("submit", submitData);
+
+function submitData(e) {
+  e.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = e.target;
+  for (let i = 1; i <= +amount.value; i += 1) {
+    const stepForPromise = +delay.value * (i - 1);
+    setTimeout(() => {
+      createPromise(i, stepForPromise).then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      });
+    }, stepForPromise);
   }
+  e.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const objectForPromise = { position, delay };
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+      resolve(objectForPromise);
+    } else {
+      // Reject
+      reject(objectForPromise);
+    }
+  });
 }
